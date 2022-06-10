5bc14a2bf576704e47e05de661510a108b631cdf
342743507 <342743507@C-2UA44529RK.tdsb.on.ca>
2022-06-10 14:54:55 -0400

    for controller class

--- BoardTestNew/src/lin/jun/Controller123_GOaddsionStopstealingtoiletpaperpleasebecausetheworldisrunninglowontreesdotoyouroverstealingoftoiletpaperalongwiththatyouneedtoreturnthatnerfgun.java
+++ BoardTestNew/src/lin/jun/Controller123_GOaddsionStopstealingtoiletpaperpleasebecausetheworldisrunninglowontreesdotoyouroverstealingoftoiletpaperalongwiththatyouneedtoreturnthatnerfgun.java
@@ -3,14 +3,26 @@ package lin.jun;
 public class Controller123_GOaddsionStopstealingtoiletpaperpleasebecausetheworldisrunninglowontreesdotoyouroverstealingoftoiletpaperalongwiththatyouneedtoreturnthatnerfgun {
 
 	public void moveRight(Shape block) {
-		
+		block.r1.setX(block.r1.getX() + 25);
+		block.r2.setX(block.r2.getX() + 25);
+		block.r3.setX(block.r3.getX() + 25);
+		block.r4.setX(block.r4.getX() + 25);
 	}
-	public void moveLeft(Shape block) {
-		
-	}
-	public void moveDown(Shape block) {
 	
+	public void moveLeft(Shape block) {
+		block.r1.setX(block.r1.getX() - 25);
+		block.r2.setX(block.r2.getX() - 25);
+		block.r3.setX(block.r3.getX() - 25);
+		block.r4.setX(block.r4.getX() - 25);
 	}
+	
+	public void moveDown(Shape block) {
+		block.r1.setY(block.r1.getY() + 25);
+		block.r2.setY(block.r2.getY() + 25);
+		block.r3.setY(block.r3.getY() + 25);
+		block.r4.setY(block.r4.getY() + 25);
+	}
+	
 	public void SMACK(Shape block) {
 	
 }
